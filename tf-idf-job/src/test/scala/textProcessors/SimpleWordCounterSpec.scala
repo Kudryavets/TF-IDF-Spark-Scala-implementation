78f247e0801d60c396e219b7bb12d652c84c1d9c
akudryavets <akudryavets@gmail.com>
2017-07-02 14:31:08 +0300

    SimpleWordCounterSpec added

--- tf-idf-job/src/test/scala/textProcessors/SimpleWordCounterSpec.scala
+++ tf-idf-job/src/test/scala/textProcessors/SimpleWordCounterSpec.scala
@@ -5,12 +5,26 @@ import org.scalatest.{Matchers, WordSpec}
 class SimpleWordCounterSpec extends WordSpec with Matchers {
   "SimpleWordCounter with DummyTextParser" should {
     "process text correctly" in {
-      val text = ???
+      val text = "Lorem ipsum dolor sit amet, 2222consectetur adipiscing elit; 777! sed do 2 eiusmod tempor incididunt ut dolor " +
+        "Lorem ipsum dolor. "
   
       val wordCounter = new SimpleWordCounter(DummyTextParser)
   
       wordCounter(text) shouldEqual Map(
-      
+        "lorem" -> 2,
+        "ipsum" -> 2,
+        "dolor" -> 3,
+        "sit" -> 1,
+        "amet" -> 1,
+        "consectetur" -> 1,
+        "adipiscing" -> 1,
+        "elit" -> 1,
+        "sed" -> 1,
+        "do" -> 1,
+        "eiusmod" -> 1,
+        "tempor" -> 1,
+        "incididunt" -> 1,
+        "ut" -> 1
       )
     }
   }
